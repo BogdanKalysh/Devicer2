8e01bef49b2309de77c1070be99d268370e2ecbe
bodyaka <bogdankalysh20@gmail.com>
2024-11-18 18:55:26 +0200

    Added start, login and sign up activities. Implemented simple logic for start activity.
    Added viewbinding to project.
    
    Temporary name to Devicer 2

---
+++ app/src/main/java/com/bkalysh/devicer2/StartActivity.kt
@@ -0,0 +1,34 @@
+package com.bkalysh.devicer2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bkalysh.devicer2.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        
+        binding.btnLogInScreen.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnSignUpScreen.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
