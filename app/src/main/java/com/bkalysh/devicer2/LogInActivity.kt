32e8ef297d638d78bacaac6c16e7dbf17e0a9403
bodyaka <bogdankalysh20@gmail.com>
2024-11-18 19:49:37 +0200

    Added basic layouts for login and sign up activities. And simple logic to jump between them.

--- app/src/main/java/com/bkalysh/devicer2/LogInActivity.kt
+++ app/src/main/java/com/bkalysh/devicer2/LogInActivity.kt
@@ -1,20 +1,31 @@
 package com.bkalysh.devicer2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bkalysh.devicer2.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_log_in)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnSignUpScreen.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/bkalysh/devicer2/SignUpActivity.kt
+++ app/src/main/java/com/bkalysh/devicer2/SignUpActivity.kt
@@ -1,20 +1,31 @@
 package com.bkalysh.devicer2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bkalysh.devicer2.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnLogInScreen.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
